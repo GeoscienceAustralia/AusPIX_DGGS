d7976aca29ae70a8329f506eec3186f48961a352
bellie888 <joseph.bell@ga.gov.au>
2019-02-26 16:09:04 +1100

    edited to convert shapefile attribute table to turtle file

--- callable_modules/call_DGGS.py
+++ callable_modules/call_DGGS.py
@@ -110,40 +110,10 @@ def poly_to_DGGS_tool(myPoly, thisRecord, resolution):  # one poly and the attri
     return insidePoly
 
 
-# find cells that can be coalesced into a bigger cell
-# makes things more complicated when using DGGS but saves on space and time somewhat ie less DGGS records
-def coalesce(dggs):  # recieves a list of dggs cell names inside a poly
 
-    newDGGSrow = list()  # declare
-
-    #calculate number of baby cells that exist
-    for aCell in dggs:
-        # go through each cell and see how many have the same parent
-        thisParent = aCell[:-1]  # parent for this cell
-        # print('thisParent', thisParent)
-
-        # count the number of cells with the same parent
-        numChilds = 0  # initialise
-        for cells in dggs:  # count the cells with the same parent
-            if cells[:-1] == thisParent:
-                numChilds += 1  #count
-
-        if numChilds == 9 : # in the case of 9 children
-            # when 9 children then only record parent
-            if thisParent not in newDGGSrow:  # ie only recorded once
-                newDGGSrow.append(thisParent)  # append parent to new list - do not record the children
-        else:  # not 9 children
-            if aCell not in newDGGSrow:
-                newDGGSrow.append(aCell)  # add the child cell (item)
-
-
-
-
-    # for item in newDGGSrow:
-    #     print(item)
-    return newDGGSrow
-
-
+if __name__ == '__main__':
+    # main
+    pass
 
 print('')
 
