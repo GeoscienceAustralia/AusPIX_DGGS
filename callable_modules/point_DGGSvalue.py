8599390c9187006cf88a394678c39d4fe3d38dca
Joseph Bell <joseph.bell@ga.gov.au>
2020-02-07 17:12:24 +1100

    Update point_DGGSvalue.py
    bit of a tidy up

--- callable_modules/point_DGGSvalue.py
+++ callable_modules/point_DGGSvalue.py
@@ -6,35 +6,24 @@ Created on 1st March 2019
 """
 
 '''
-small module to find the DGGS cell for a point (long, lat) 
-
+small module to find the DGGS cell for a point (long, lat) WGS84
 '''
 from AusPIXengine.dggs import RHEALPixDGGS
 rdggs = RHEALPixDGGS()
 
 def latlong_to_DGGS(coords, resolution):
 
-    # Pick a (longitude-latitude) point on the ellipsoid and find the resolution cell that contains it ::
-    # coords = (longi, lati)  # format required like this
-
-    # set the resolution
-    #resolution = 10
-    # calculate the dggs cell from long and lat ie t
+    # calculate the dggs cell from long and lat
     thisCell = rdggs.cell_from_point(resolution, coords, plane=False)  # false = on the elipsoidal curve
 
     # now have a dggs cell for that point
     print('DGGScell', thisCell)
     return thisCell
 
-
-
 if __name__ == '__main__':
-    # main
-    #SA1
     coordinates = (148.9668333, -35.38275)
     print('test values', coordinates)
-    answer = latlong_to_DGGS(coordinates, 12)
+    answer = latlong_to_DGGS(coordinates, 12)  #12 is the resolution
 
     print('answer', answer)
-
     print('finished')
