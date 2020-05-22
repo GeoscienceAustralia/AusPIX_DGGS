5265ea54fd814ff7ab3fca92b8342370c0920dbc
Jonathan Yu <jonathan.yu@csiro.au>
2020-05-22 12:26:29 +1000

    add melb cbd multiple poly test

--- tests/enablement/test_geojson_to_dggs.py
+++ tests/enablement/test_geojson_to_dggs.py
@@ -61,6 +61,30 @@ def test_NSW_SA1_sydney_haymarket_geojson_to_DGGS():
         assert set(test_dggs_lvl10_cells).issubset(set(cells)) == True       
 
 
+def test_VIC_SA1_Melbourne_CBD_multiple_to_DGGS():
+    # read in the file
+    geojson = get_geojson('test_data/VIC_SA1_Melbourne_CBD_multiple.geojson')
+    list_cells = []
+
+    for fea in geojson['features']:  # for feature in attribute table        
+        polygon = geojson_to_shape(fea['geometry'])
+        thisbbox = bbox(list(coords(fea)))
+        cells = poly_to_DGGS_tool(polygon, '', 10, input_bbox=thisbbox)  # start at DGGS level 10
+
+        for item in cells:
+             print(item)
+             if(isinstance(item, list)):
+                list_cells.append(item[0])                 
+                
+             else:
+                list_cells.append(item)
+        #list_cells = list_cells + cells
+
+    test_dggs_lvl10_cells = ['R7847710830', 'R7847710752']
+    print(list_cells)
+    assert set(test_dggs_lvl10_cells).issubset(set(list_cells)) == True       
+
 if __name__ == "__main__":
     #test_ABS_SA1_shp_to_DGGS()
-    test_ABS_SA1_black_mountain_geojson_to_DGGS()
+    #test_ABS_SA1_black_mountain_geojson_to_DGGS()
+    test_VIC_SA1_Melbourne_CBD_multiple_to_DGGS()
