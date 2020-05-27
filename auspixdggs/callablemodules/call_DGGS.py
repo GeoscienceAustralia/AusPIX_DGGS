bc1213ab17efd8c8b0270e7c7c4617e5345e1b5c
Jonathan Yu <jonathan.yu@csiro.au>
2020-05-27 11:43:22 +1000

    Adding a LineString and MultiLineString test and func for geojson

--- auspixdggs/callablemodules/call_DGGS.py
+++ auspixdggs/callablemodules/call_DGGS.py
@@ -13,7 +13,7 @@ Joseph Bell GeoScience Australia
 
 from ..auspixengine.dggs import RHEALPixDGGS
 
-from shapely.geometry import shape, Point, Polygon  # used in the function below ??
+from shapely.geometry import shape, Point, Polygon, MultiLineString, LineString  # used in the function below ??
 # import csv
 
 
@@ -116,7 +116,26 @@ def poly_to_DGGS_tool(myPoly, thisRecord, resolution, input_bbox=None):  # one p
     # return the cells inside the polygon at this DGGS resolution
     return insidePoly
 
-
+def line_to_DGGS(myLineObj, resolution):  # one poly and the attribute record for it
+    """
+    Takes a line and a resolution and returns a list of DGGS cells objects.
+    myLineObj is expected to be a shapely MultiLineString or LineString object.    
+    """
+    doneDGGScells = [] #to accumlate a list of completed cells
+    arrLines = []
+    if(isinstance(myLineObj, MultiLineString)):        
+        arrLines = list(myLineObj)
+    else: #assume this is a LineString object
+        arrLines.append(myLineObj)
+    #iterate through list of LineStrings    
+    for currLine in arrLines:
+        for pt in currLine.coords:  # for each point calculate the DGGS by calling on the DGGS engine
+            # ask the engine what cell thisPoint is in
+            thisDGGS = rdggs.cell_from_point(resolution, pt, plane=False)# plane=false therefore on the ellipsoid curve
+            #add cell if not already in there
+            if thisDGGS not in doneDGGScells: # == new one
+                doneDGGScells.append(thisDGGS) # save as a done cell
+    return doneDGGScells
 
 if __name__ == '__main__':
     # main
--- tests/enablement/test_geojson_to_dggs.py
+++ tests/enablement/test_geojson_to_dggs.py
@@ -2,7 +2,7 @@ import pytest
 import shapefile
 from pyproj import Proj, transform
 import json
-from auspixdggs.callablemodules.call_DGGS import poly_to_DGGS_tool
+from auspixdggs.callablemodules.call_DGGS import poly_to_DGGS_tool, line_to_DGGS
 import geojson
 from geojson.utils import coords
 from shapely.geometry import Polygon, shape
@@ -111,9 +111,30 @@ def try_SA1_flinders_Cape_Barren_multipoly_to_DGGS():
     print(list_cells)
     assert set(test_dggs_lvl10_cells).issubset(set(list_cells)) == True       
 
+def test_geojson_line_data():
+    # read in the file
+    geojson = get_geojson_by_file('test_data/SA_RoadsExample_lines.geojson')
+    list_cells = []
+
+    for fea in geojson['features']:  # for feature in attribute table        
+        geom = geojson_to_shape(fea['geometry'])
+        cells = line_to_DGGS(geom, 10)  # start at DGGS level 10
+
+        for item in cells:
+             #print(item)
+             if(isinstance(item, list)):
+                list_cells.append(str(item[0]))                
+             else:
+                list_cells.append(str(item))
+        #list_cells = list_cells + cells
+
+    test_dggs_lvl10_cells = ['R7751215231', 'R7751215230', 'R7751223885', 'R7751218502']
+    print(list_cells)
+    assert set(test_dggs_lvl10_cells).issubset(set(list_cells)) == True    
 
 if __name__ == "__main__":
     #test_ABS_SA1_shp_to_DGGS()
-    test_ABS_SA1_black_mountain_geojson_to_DGGS()
+    #test_ABS_SA1_black_mountain_geojson_to_DGGS()
     #test_VIC_SA1_Melbourne_CBD_multiple_to_DGGS()
     #test_SA1_flinders_Cape_Barren_multipoly_to_DGGS()
+    test_geojson_line_data()
