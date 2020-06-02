b314c2496fff2d53a13274bad0c2b49767a31a7d
Jonathan Yu <jonathan.yu@csiro.au>
2020-06-02 15:19:37 +1000

    remove dependency on shapely for dggs_in_line func

--- auspixdggs/callablemodules/dggs_in_line.py
+++ auspixdggs/callablemodules/dggs_in_line.py
@@ -1,9 +1,10 @@
 
 from ..auspixengine.dggs import RHEALPixDGGS
-from shapely.geometry import shape, MultiLineString, LineString 
+#from shapely.geometry import shape, MultiLineString, LineString 
 
 rdggs = RHEALPixDGGS()
 
+'''
 def line_to_DGGS(myLineObj, resolution):  # one poly and the attribute record for it
     """
     Takes a line and a resolution and returns a list of DGGS cells objects.
@@ -23,4 +24,19 @@ def line_to_DGGS(myLineObj, resolution):  # one poly and the attribute record fo
             #add cell if not already in there
             if thisDGGS not in doneDGGScells: # == new one
                 doneDGGScells.append(thisDGGS) # save as a done cell
+    return doneDGGScells
+'''
+
+def line_to_DGGS(line_coords, resolution):  # one poly and the attribute record for it
+    """
+    Takes a list of line coords and a resolution and returns a list of DGGS cells objects.
+    """
+    doneDGGScells = [] #to accumlate a list of completed cells
+    arrLines = []
+    for pt in line_coords:  # for each point calculate the DGGS by calling on the DGGS engine
+        # ask the engine what cell thisPoint is in
+        thisDGGS = rdggs.cell_from_point(resolution, pt, plane=False)# plane=false therefore on the ellipsoid curve
+        #add cell if not already in there
+        if thisDGGS not in doneDGGScells: # == new one
+            doneDGGScells.append(thisDGGS) # save as a done cell
     return doneDGGScells
